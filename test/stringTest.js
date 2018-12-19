23205dfccd0fa6f489e4049b80eb4a29bb1b6360
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-19 22:52:18 +0530

    added seperate it blocks fo the respective tests in stringTest

--- test/stringTest.js
+++ test/stringTest.js
@@ -1,78 +1,86 @@
-const { equal, deepEqual } = require("assert");
+const assert = require("assert");
 
 const { head, tail, headerText } = require("../src/util/string.js");
 
 describe("head", () => {
-  it("it should return nothing when we give 0 as input ", () => {
-    equal(head("leela", 0, "\n"), "");
-    equal(head("prasanth", 0, ""), "");
+  it("it should return nothing when we give 0 and \n as input ", () => {
+    assert.equal(head("leela", 0, "\n"), "");
   });
 
-  it("it should return one line if we give 1 as input", () => {
-    equal(head("leela\nprasanth", 1, "\n"), "leela");
-    equal(head("head", 1, ""), "h");
+  it("it should return nothing when we give 0 and empty string as input ", () => {
+    assert.equal(head("prasanth", 0, ""), "");
+  });
+
+  it("it should return first line if we give 1 and \n  as input", () => {
+    assert.equal(head("leela\nprasanth", 1, "\n"), "leela");
+  });
+
+  it("it should return first line if we give 1 and empty string as input", () => {
+    assert.equal(head("head", 1, ""), "h");
   });
 
   it("it should return those many number of lines as per input", () => {
-    equal(head("leela\nprasanth\nnakka", 2, "\n"), "leela\nprasanth");
-    equal(head("head", 4, ""), "head");
+    assert.equal(head("leela\nprasanth\nnakka", 2, "\n"), "leela\nprasanth");
+    assert.equal(head("head", 4, ""), "head");
   });
 
   it("it should return those many number of lines as per input", () => {
-    let input =
-      "Ever man are put down his very And marry may table him avoid\nHard sell it were into it upon\nHe forbade affixed parties of assured to me windows";
-    let expectedOutPut =
-      "Ever man are put down his very And marry may table him avoid\nHard sell it were into it upon";
-    equal(head(input, 2, "\n"), expectedOutPut);
-    equal(head("h\ne\na\nd", 4, ""), "h\ne\n");
+    let input = "If you\nspend a\nlot of\nyour day\nat the\ncommand line";
+    assert.equal(head(input, 2, "\n"), "If you\nspend a");
+    assert.equal(head(input, 4, ""), "If y");
   });
 });
 
 describe("tail", () => {
-  it("should return the last character of the file", () => {
-    equal(tail("leela", 1, ""), "a");
-    equal(tail("leela\nprasanth", 1, "\n"), "prasanth");
+  it("should return the last character of the file for the delimiter as empty string", () => {
+    assert.equal(tail("leela", 1, ""), "a");
   });
 
-  it("should return the content based on the delimiter", () => {
+  it("should return the last line of the file for the delimiter as \n", () => {
+    assert.equal(tail("leela\nprasanth", 1, "\n"), "prasanth");
+  });
+
+  it("should return the bit wise content for empty string as the delimeter", () => {
     let input =
-      "Ever man are put down his very And marry may table him avoid\nHard sell it were into it upon\nHe forbade affixed parties of assured to me windows";
-    equal(tail(input, 2, ""), "ws");
-    equal(tail(input, 9, ""), "e windows");
-    equal(
-      tail(input, 1, "\n"),
-      "He forbade affixed parties of assured to me windows"
-    );
-    equal(
-      tail(input, 2, "\n"),
-      "Hard sell it were into it upon\nHe forbade affixed parties of assured to me windows"
-    );
+      "Ever man\nare put\ndown his\nvery And\nmarry may\ntable him\navoid";
+    assert.equal(tail(input, 2, ""), "id");
+    assert.equal(tail(input, 9, ""), "him\navoid");
+  });
+
+  it("should return the line wise content for \n as the delimiter", () => {
+    let input = "Ever\nman\nare\nput\ndown\nhis\nvery\nAnd\nmarry\nmay";
+    assert.equal(tail(input, 1, "\n"), "may");
+    assert.equal(tail(input, 2, "\n"), "marry\nmay");
   });
 
   it("should tail the contents based on the number and delimiter as \n", () => {
-    equal(tail("h\ne\na\nd", 2, "\n"), "a\nd");
+    assert.equal(tail("h\ne\na\nd", 2, "\n"), "a\nd");
   });
+
   it("should tail the contents based on the number and delimiter as empty string ", () => {
-    equal(tail("h\ne\na\nd", 2, ""), "\nd");
+    assert.equal(tail("h\ne\na\nd", 2, ""), "\nd");
   });
+
   it("should trim the empty line if string contains empty line at the end", () => {
-    equal(tail("t\na\ni\nl", 2, "\n"), "i\nl");
-    equal(tail("tail\nhead", 1, "\n"), "head");
+    assert.equal(tail("t\na\ni\nl", 2, "\n"), "i\nl");
+    assert.equal(tail("tail\nhead", 1, "\n"), "head");
   });
 });
 
 describe("headerText", () => {
   it("should return dafault header for no input file name", () => {
-    equal(headerText(""), "==> " + "" + " <==");
+    assert.equal(headerText(""), "==> " + "" + " <==");
   });
 
   it("should reaturn the header text with the input file name", () => {
-    equal(headerText("head.js"), "==> " + "head.js" + " <==");
+    assert.equal(headerText("head.js"), "==> " + "head.js" + " <==");
   });
+
   it("should return the header text with the file name", () => {
-    equal(headerText("file"), "==> file <==");
+    assert.equal(headerText("file"), "==> file <==");
   });
+
   it("should return the header text with empty if user gives no file name", () => {
-    equal(headerText(""), "==>  <==");
+    assert.equal(headerText(""), "==>  <==");
   });
 });
