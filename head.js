d38cb3168718cee0096b34db956d1c507c448c54
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-12-19 18:07:26 +0530

    changed in head.js function passed the correct arg

--- head.js
+++ head.js
@@ -2,4 +2,4 @@ const { getContent } = require("./src/lib.js");
 
 const fs = require("fs");
 
-console.log(getContent(process.argv.slice(2), fs, "tail"));
+console.log(getContent(process.argv.slice(2), fs, "head"));
